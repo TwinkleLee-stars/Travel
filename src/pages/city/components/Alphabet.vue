2d1e061b73af13df373cf6914ad97e2faf9b6efa
xinxin.li <twinkleLee_stars@163.com>
2019-11-23 16:58:11 +0800

    city页添加右侧字母列表
    1.用flex布局做垂直居中
    display:flex
    flex-direction: column
    justify-content: center

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="list">
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  
+  .list
+    position: absolute
+    right: 0
+    top: 1.77rem
+    bottom: 0
+    width: .4rem
+    display: flex
+    flex-direction: column
+    justify-content: center
+  .item
+    text-align: center
+    line-height: .4rem
+    color: $bgColor
+</style>
