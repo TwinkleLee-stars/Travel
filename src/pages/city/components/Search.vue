e5b5031484fc81550d924b808986e3d4df7fa072
xinxin.li <twinkleLee_stars@163.com>
2019-11-24 17:42:45 +0800

    city页面search组件逻辑实现完成
    1.用setTimeout函数实现节流

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,60 @@
 <template>
-  <div class="search">
-    <input type="text" class="search-input" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input type="text" class="search-input" placeholder="输入城市名或拼音"
+        v-model="content"
+      >
+    </div>
+    <div ref="searchlist" class="search-list" v-show="content">
+      <ul>
+        <li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+        <li class="search-item" v-show="hasNoContent">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+
 export default{
-  name: 'CitySearch'
+  name: 'CitySearch',
+  data () {
+    return {
+      content: '',
+      list: [],
+      timer: null
+    }
+  },
+  props: {
+    cities: Object
+  },
+  watch: {
+    content () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.content) > -1 || value.name.indexOf(this.content) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  computed: {
+    hasNoContent () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.searchlist)
+  }
 }
 </script>
 
@@ -27,4 +75,18 @@ export default{
       text-align: center
       color: #666
       padding 0 .1rem
+  .search-list
+    background-color: #eee
+    position: absolute
+    top: 1.77rem
+    bottom: 0
+    left: 0
+    right: 0
+    z-index: 1
+    overflow: hidden
+    .search-item
+      padding-left: .2rem
+      line-height: .62rem
+      color: #666
+      background-color: #fff
 </style>
