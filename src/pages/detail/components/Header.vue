54a1cce50cd21172c404a29796eff0ffde3b860a
xinxin.li <twinkleLee_stars@163.com>
2020-01-09 19:43:17 +0800

    detail页面header组件完成
    1.document.documentElement.scrollTop
    2.window.addEventListener('',...)
    要用window.removeEventListener('',...)取消侦听，否则会影响其他页面

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,12 +1,74 @@
 <template>
-  <div></div>
+  <div>
+    <router-link to="/" tag="div" class="back-icon-wrapper" v-show="!isShow" >
+      <span class="iconfont back-icon"> &#xe8b5; </span>
+    </router-link>
+    <div class="header" v-show="isShow" :style="style">景点详情</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'DetailHeader'
+  name: 'DetailHeader',
+  data () {
+    return {
+      isShow: false,
+      style: {
+        opacity: 1
+      }
+    }
+  },
+  methods: {
+    changeIsShow () {
+      const top = document.documentElement.scrollTop 
+      if (top > 60) {
+        this.isShow = true
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+      } else {
+        this.isShow = false
+      }
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.changeIsShow)
+  },
+  unmounted () {
+    window.removeEventListener('scroll', this.changeIsShow)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+
+  .header
+    position: fixed
+    top:0
+    left:0
+    width: 100%
+    background-color: $bgColor
+    height: $headerHeight
+    line-height: $headerHeight
+    font-size: .32rem
+    color: #fff
+    text-align: center
+
+  .back-icon-wrapper
+    width: .8rem
+    height: .8rem
+    border-radius: .4rem
+    background: #000
+    position: absolute
+    top: .2rem
+    left: .2rem
+    z-index: 99
+    .back-icon
+      color: #fff;
+      position: relative;
+      top: .1rem;
+      left: .24rem;
+      font-size: 0.6rem
 </style>
